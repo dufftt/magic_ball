0be4fc33a1a2977b85d62ef135bddb04eadf447f
Sourav <rockythere81@gmail.com>
2020-08-17 23:05:34 +0530

    uploading

---
+++ android/app/src/main/kotlin/com/duft/magic_ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.duft.magic_ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.lightBlueAccent,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int number = 1;
+  void changedice() {
+    setState(() {
+      number = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changedice();
+              },
+              child: Image.asset('images/ball$number.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
